2082fdba63fe6be9bb5b8716639cbc4aaf8c427b
balraj hanmanthugari <balraj.hanmanthugari@gmail.com>
2025-01-03 17:03:18 +0530

    form builder and fork join

--- src/app/student/student-list/student-list.component.ts
+++ src/app/student/student-list/student-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StudentService } from '../student.service';
 import { SelectionChangedEvent } from 'ag-grid-community';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs';
 
 @Component({
@@ -18,18 +18,19 @@ export class StudentListComponent implements OnInit {
   deleteLabel: String = "Delete";
   deleteDisabled: Boolean = true;
 
+  searchForm: any;
   studentList: any[] = [];
 
-  searchForm = new FormGroup({
-    name: new FormControl('')
-  });
 
-  constructor(private studentService: StudentService, private route: ActivatedRoute,
+  constructor(private studentService: StudentService, private route: ActivatedRoute, private formBuilder: FormBuilder,
     private router: Router) {
 
   }
 
   ngOnInit(): void {
+    this.searchForm = this.formBuilder.group({
+      name: new FormControl('', Validators.required)
+    });
     this.searchForm.controls.name.valueChanges.pipe(
       debounceTime(300),
       map(name => name),
@@ -39,12 +40,12 @@ export class StudentListComponent implements OnInit {
       next: (data: any) => {
         this.studentList = data.data.students;
       },
-      error: (e) => console.error(e)
+      error: (e: Error) => console.error(e)
     });
   }
 
   editSelectedStudent(): void {
-
+    console.log(this.searchForm);
   }
 
   deleteSelectedStudent(): void {
--- src/app/student/student.service.ts
+++ src/app/student/student.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Student } from './student.model';
-import { BehaviorSubject, catchError, debounceTime, distinctUntilChanged, ReplaySubject, Subject, switchMap, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, debounceTime, distinctUntilChanged, forkJoin, ReplaySubject, Subject, switchMap, throwError } from 'rxjs';
 
 //const baseUrl = 'http://localhost:8080/ems/v1/student';
 const baseUrl = 'http://localhost:3000/student'
@@ -51,5 +51,14 @@ export class StudentService {
 
   delete(id: any): Observable<any> {
     return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  forkJoinEx() {
+    let req1 = this.http.get(`${baseUrl}/1`);
+    let req2 = this.http.get(`${baseUrl}/2`);
+    forkJoin([req1, req2]).subscribe(([resp1, resp2]) => {
+      console.log(resp1);
+      console.log(resp2);
+    });
   }
 }
